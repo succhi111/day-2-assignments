39cfcd25a07071c688c16225c8593ce709fdb9c9
succhi111 <suchendramishra396@gmail.com>
2022-08-27 20:03:02 +0530

    program to get place value of a number

--- src/NumPlace.java
+++ src/NumPlace.java
@@ -3,27 +3,26 @@ import java.util.Scanner;
 public class NumPlace {
     public static void main(String[] args) {
         System.out.println("enter any number to get the place value");
-        Scanner r= new Scanner(System.in);
-        int num=r.nextInt();
-        if (num<10)
+        Scanner r = new Scanner(System.in);
+        int num = r.nextInt();
+
+        if (num < 10)
             System.out.println("Unit");
 
-        else if (num<100)
+        else if (num < 100)
             System.out.println("tenth");
 
-            else if (num<1000)
+        else if (num < 1000)
             System.out.println("Hundred");
 
 
-            else if (num<10000)
-                System.out.println("Thousand");
+        else if (num < 10000)
+            System.out.println("Thousand");
 
-         else
-
+        else
             System.out.println("plese enter valid number");
 
-            
-        }
     }
+}
     
 
