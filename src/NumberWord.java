ac696a32cba5d99e7c57038985c0c8aa32082398
succhi111 <suchendramishra396@gmail.com>
2022-08-27 14:21:56 +0530

    program to get number to words of a single digit

--- src/NumberWord.java
+++ src/NumberWord.java
@@ -6,25 +6,26 @@ public class NumberWord {
         if (num == 1)
             System.out.println("one");
         else if (num == 2)
-                System.out.println("two");
-            else if (num == 3)
-                System.out.println("three");
-            else if (num == 4)
-                System.out.println("four");
-            else if (num == 5)
-                System.out.println("five");
-            else if (num == 6)
-                System.out.println("six");
-            else if (num == 7)
-                System.out.println("seven");
-            else if (num == 8)
-                System.out.println("eight");
-            else if (num == 9)
-                System.out.println("mine");
-            else if (num == 0)
-                System.out.println("zero");
-        }
-
+            System.out.println("two");
+        else if (num == 3)
+            System.out.println("three");
+        else if (num == 4)
+            System.out.println("four");
+        else if (num == 5)
+            System.out.println("five");
+        else if (num == 6)
+            System.out.println("six");
+        else if (num == 7)
+            System.out.println("seven");
+        else if (num == 8)
+            System.out.println("eight");
+        else if (num == 9)
+            System.out.println("mine");
+        else if (num == 0)
+            System.out.println("zero");
+        else
+            System.out.println("please enter single digit from 0to 9");
+    }
 
 
     public static void main(String[] args) {
