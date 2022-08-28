e6bb23d1ff890437091e469744578ee76ab31abe
succhi111 <suchendramishra396@gmail.com>
2022-08-28 11:13:44 +0530

    program to find vowel using switch case

---
+++ src/SwitchVowel.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class SwitchVowel {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an alphabet: ");
+        char ch = sc.next().charAt(0);
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println(ch + " is a vowel.");
+                break;
+            default:
+                System.out.println(ch + " is a consonant.");
+        }
+    }
+}
