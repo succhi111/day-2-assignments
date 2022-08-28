68ef156c97a53ee5b9bb3d0abe261267585f7b6a
succhi111 <suchendramishra396@gmail.com>
2022-08-28 09:48:36 +0530

    program to get palindrome number using for loop

--- src/PalindromeFor.java
+++ src/PalindromeFor.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
- public class PalindromeFor {
+
+public class PalindromeFor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number : ");
-        int n= sc.nextInt();
-        int c=n;
-        int r=0;
-        int s=0;
-        for (; n>0; ) {
-            r = n% 10;
+        int n = sc.nextInt();
+        int c = n;
+        int r = 0;
+        int s = 0;
+        for (; n > 0; ) {
+            r = n % 10;
             s = s * 10 + r;
             n = n / 10;
         }
-        if (c==s)
+        if (c == s)
             System.out.println("Entered number is a palindrome.");
         else
 
